5e5a3d1af8ce564eae78bde1b4b0bbd31fac0582
gabriel <gab.ge.silva74@gmail.com>
2024-04-08 18:08:46 -0300

    new database

--- src/database/knex/migrations/20240323134256_createLinks.js
+++
@@ -1,8 +0,0 @@
-exports.up = knex => knex.schema.createTable("links", table => {
-    table.increments("id");
-    table.text("url").notNullable(); //Obrigatório que o campo nome não pode ser nulo.
-    table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");//O onDelete é uma função que se a nota for excluída o link também será.
-    table.timestamp("created_at").default(knex.fn.now());
-  });
-
-exports.down = knex => knex.schema.dropTable("links");
