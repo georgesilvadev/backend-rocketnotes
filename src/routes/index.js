01e2b90ebdc42d4a0649bfc69164684df7c1f0d7
gabriel <gab.ge.silva74@gmail.com>
2024-08-23 17:03:15 -0300

    authentication routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,16 +4,18 @@
 const { Router } = require("express")
 
 //importando o userRoutes
-const usersRoutes = require("./users.routes")
-const notesRoutes = require("./notes.routes")
 const tagsRoutes = require("./tags.routes")
+const notesRoutes = require("./notes.routes")
+const usersRoutes = require("./users.routes")
+const sessionsRoutes = require("./sessions.routes")
 
 //inicialização do Router
 const routes = Router();
 
 //Quando foi solicitado o caminho /users, utilizar a rota usersRoutes
-routes.use("/users", usersRoutes);
-routes.use("/notes", notesRoutes);
 routes.use("/tags", tagsRoutes);
+routes.use("/notes", notesRoutes);
+routes.use("/users", usersRoutes);
+routes.use("/sessions", sessionsRoutes);
 
 module.exports = routes;
